b4e15ceb223cc7257e5e398fb89c1864d10109e1
devsantos1999 <devsantos1999@gmail.com>
2025-03-23 17:00:44 -0300

    adding social networks to the home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,5 @@
 import profilePhoto from '../assets/profile-photo.jpg';
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 
 function Home() {
@@ -12,6 +13,17 @@ function Home() {
                 <p className='text-gray-400'>Sou formado em Análise e Desenvolvimento de Sistemas pela UniRitter e iniciei minha trajetória em 2019, desenvolvendo software para automação de processos internos na própria instituição. Esse projeto não apenas fortaleceu minhas habilidades técnicas, como também viabilizou minha graduação, uma vez que fui contemplado com o pagamento integral do curso pelas atividades prestadas. 
                     Em 2022, após concluir minha graduação, ingressei na Altus, onde atuo como Assistente de Produtos, sendo responsável pela manutenção e evolução de sistemas robustos de automação para PLCs, incluindo softwares como MasterTool IEC XE, ArchiteX e soluções parceiras como BCSTools.
                     Atualmente, estou ampliando meus conhecimentos por meio de uma pós-graduação em Engenharia de Software na Unisinos. Estou em transição da senioridade Júnior para Pleno, aprimorando minhas habilidades para o mercado de software de alto nível..</p>
+                <div className='flex space-x-4'>
+                    <a href="https://github.com/devSantos16" target="_blank" rel="noopener noreferrer">
+                        <FaGithub className="text-white text-3xl transition-colors duration-300 hover:text-gray-400" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/devsantos16/" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin className="text-white text-3xl transition-colors duration-300 hover:text-blue-600" />
+                    </a>
+                    <a href="https://www.instagram.com/vulgo_hunkzin/" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram className="text-white text-3xl transition-colors duration-300 hover:text-pink-500" />
+                    </a>
+                </div>
             </div>
             <div className='max-w-xs'>
                 <img className="shadow rounded" src={profilePhoto} alt="My photo" />
