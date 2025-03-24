4e2c5afc6438de70d578dfab91f6d6dcea0ecc96
devsantos1999 <devsantos1999@gmail.com>
2025-03-24 16:27:17 -0300

    removing the last “.”

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,7 +12,7 @@ function Home() {
                 </div>
                 <p className='text-gray-400'>Sou formado em Análise e Desenvolvimento de Sistemas pela UniRitter e iniciei minha trajetória em 2019, desenvolvendo software para automação de processos internos na própria instituição. Esse projeto não apenas fortaleceu minhas habilidades técnicas, como também viabilizou minha graduação, uma vez que fui contemplado com o pagamento integral do curso pelas atividades prestadas. 
                     Em 2022, após concluir minha graduação, ingressei na Altus, onde atuo como Assistente de Produtos, sendo responsável pela manutenção e evolução de sistemas robustos de automação para PLCs, incluindo softwares como MasterTool IEC XE, ArchiteX e soluções parceiras como BCSTools.
-                    Atualmente, estou ampliando meus conhecimentos por meio de uma pós-graduação em Engenharia de Software na Unisinos. Estou em transição da senioridade Júnior para Pleno, aprimorando minhas habilidades para o mercado de software de alto nível..</p>
+                    Atualmente, estou ampliando meus conhecimentos por meio de uma pós-graduação em Engenharia de Software na Unisinos. Estou em transição da senioridade Júnior para Pleno, aprimorando minhas habilidades para o mercado de software de alto nível.</p>
                 <div className='flex space-x-4'>
                     <a href="https://github.com/devSantos16" target="_blank" rel="noopener noreferrer">
                         <FaGithub className="text-white text-3xl transition-colors duration-300 hover:text-gray-400" />
