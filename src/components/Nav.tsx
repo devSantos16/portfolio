6bff6307c5f9c8459d096f24d8867f4cc5d009ef
devsantos1999 <devsantos1999@gmail.com>
2025-03-24 16:26:41 -0300

    adjusting the nav component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,20 @@
-import PurpleButton from "./PurpleButton";
+import PurpleButton from "./PurpleButton"
+
+
+function DownloadPdf(download: string) {
+    const link = document.createElement('a')
+    link.href = download
+    link.download = "curriculum.docx"
+    link.click()
+}
+
 
 function Nav() {
     return (
         <nav className="flex flex-wrap text-white justify-between items-center py-10 px-20">
-            <h1 className="text-lg font-bold">Emanuel dos Santos Paz</h1>
+            <h1 className="text-2xl font-bold">Portfólio em Desenvolvimento</h1>
             <div className="flex items-center space-x-6 text-sm">
-                <ul className="flex space-x-6">
-                    <li>Home</li>
-                    <li>Carreira</li>
-                    <li>Projetos</li>
-                    <li>Contatos</li>
-                </ul>
-                <PurpleButton>Meu currículo</PurpleButton>
+                <PurpleButton OnClick={() => DownloadPdf("/assets/curriculum/curriculum.docx")}>Meu currículo</PurpleButton>
             </div>
         </nav>
     );
