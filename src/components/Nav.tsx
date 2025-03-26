33d771cad098a6c4e2899fd70c258a4cdb9cb310
devsantos1999 <devsantos1999@gmail.com>
2025-03-26 16:08:03 -0300

    sending the curriculum.docx to the public

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,11 @@
 import PurpleButton from "./PurpleButton"
 
-
-function DownloadPdf(download: string) {
-    const link = document.createElement('a')
-    link.href = download
-    link.download = "curriculum.docx"
-    link.click()
-}
-
-
 function Nav() {
     return (
         <nav className="flex flex-wrap gap-5 text-white justify-between items-center p-10">
             <h1 className="font-bold">Portfólio em Desenvolvimento</h1>
             <div className="flex items-center text-sm">
-                <PurpleButton OnClick={() => DownloadPdf("/assets/curriculum.docx")}>Meu currículo</PurpleButton>
+                <PurpleButton OnClick={() => window.location.href = "/curriculum.docx"}>Meu currículo</PurpleButton>
             </div>
         </nav>
     );
